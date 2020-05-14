889a6b17ff2c0e1431a81b0a5f4a243bc59826cd
CargoElf <benjamin.m.vansickle@gmail.com>
2020-05-14 13:42:36 -0500

    add basic tests

---
+++ spec/graph_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../graph.rb'
+
+RSpec.describe Graph do
+  describe 'Attributes' do
+
+  end
+
+  describe '#sort' do
+    it 'sorts nodes correctly' do
+
+    end
+
+    it 'throws an error if two nodes have same name' do
+
+    end
+
+    it 'throws an error if there is a circular path' do
+      
+    end
+  end
+end
